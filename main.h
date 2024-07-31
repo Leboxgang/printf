5cb99eed5050a79a71240eeb339f67011bcb4d50
Leboxgang <thevirusbruh@gmail.com>
2024-07-31 09:29:02 -0700

    main.h

--- main.h
+++ main.h
@@ -8,5 +8,9 @@ int _printf(const char *format, ...);
 int print_char(va_list args);
 int print_string(va_list args);
 int print_percent(va_list args);
+int handle_char(va_list args);
+int handle_string(va_list args);
+int print_number(int num);
+int handle_percent(void);
 
 #endif
