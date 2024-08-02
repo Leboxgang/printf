4e260071f95fdf4b40e870e00bf6ae09ac8b2646
Leboxgang <thevirusbruh@gmail.com>
2024-08-02 07:20:14 -0700

    binary

--- main.c
+++ main.c
@@ -12,6 +12,7 @@ int main(void)
         _printf("Hello, %s!\n", "World");
         _printf("Character: %c\n", 'A');
         _printf("Percentage: %%\n");
+	_printf("%b\n", 98);
 
 	return (0);
 }
--- printf_binary.c
+++ printf_binary.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include "main.h"
 
 /* Prototype for the custom _printf function */
 int _printf(const char *format, ...);
